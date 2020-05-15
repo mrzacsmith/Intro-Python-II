0e353f2466e8b58cea727e9c275f3a4634596c5b
Zac Smith <zrs3141592@gmail.com>
2020-05-15 02:00:13 -0600

    create display, add and drop items

--- src/room.py
+++ src/room.py
@@ -12,6 +12,21 @@ class Room:
         self.e_to = e_to
         self.s_to = s_to
         self.w_to = w_to
+        self.items = []
 
     def __str__(self):
         return f'{self.name} {self.description}'
+
+    def displayItems(self):
+        for item in self.items:
+            print(item)
+
+    def addItems(self, *newItems):
+        for item in newItems:
+            self.items.append(item)
+
+    def dropItems(self, item):
+        if item in self.items:
+            self.items.remove(item)
+        else:
+            print(f'{item} can not be dropped in this room ')
